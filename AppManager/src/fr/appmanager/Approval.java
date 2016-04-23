b67e0cb9d231941a7189ac4458a744f70d60ef48
Pierre <pier.charles@hotmail.fr>
2016-04-23 14:40:16 +0200

    Refactoring all project

---
+++ AppManager/src/fr/appmanager/Approval.java
@@ -0,0 +1,50 @@
+package fr.appmanager;
+
+public class Approval {
+
+	private String name;
+	private String manual_Response;
+
+	/**
+	 * Getter of the name
+	 * @return
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * Setter of the name
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Getter of manual_Response
+	 * @return
+	 */
+	public String getManual_Response() {
+		return this.manual_Response;
+	}
+
+	/**
+	 * Setter of manual_Response
+	 * @param manual_Response
+	 */
+	public void setManual_Response(String manual_Response) {
+		this.manual_Response = manual_Response;
+	}
+
+	/**
+	 * Constructor of one approval
+	 * @param name
+	 * @param manual_response
+	 */
+	public Approval(String name, String manual_response) {
+		this.name = name;
+		this.manual_Response = manual_response;
+	}
+
+}
