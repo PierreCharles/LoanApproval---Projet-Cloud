06fc55a27c4a866f859ba3a17b858a11056e13ef
julien <bratjulien@gmail.com>
2016-04-29 23:57:36 +0200

    Silex base OKAY

--- client-loanApproval/index.php
+++ client-loanApproval/index.php
@@ -3,6 +3,10 @@ require_once 'vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+$app->get('/', function() {
+    return "hello";
+});
+
 $app->get('/home/', function() { 
     return 'Hello '; 
 }); 
