30a5a1bb019f5bde389c4a874650d201feb9d2f6
julien <bratjulien@gmail.com>
2016-04-30 00:33:19 +0200

    ajout POJO et librairie jersey manquante

---
+++ AccManager/src/fr/accManager/AccManager.java
@@ -0,0 +1,21 @@
+package fr.accManager;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/bankAccount")
+public class AccManager {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getBankAccount() {
+		return "{\"message\" : \"Hello\"}";
+	}
+
+	
+}
---
+++ AccManager/src/fr/accManager/model/BankAccount.java
@@ -0,0 +1,77 @@
+package fr.accManager.model;
+
+public class BankAccount 
+{
+	/**
+	 * Name of client
+	 */
+	private String lastName;
+	
+	/**
+	 * First name of the client
+	 */
+	private String firstName;
+	
+	/**
+	 * Account id
+	 */
+	private String account;
+	
+	/**
+	 * Risk of the account value
+	 */
+	private String risk;
+
+	/**
+	 * Constructor of the class Bank account 
+	 * 
+	 * @param lastName
+	 * @param firstName
+	 * @param account
+	 * @param risk
+	 */
+	public BankAccount(String lastName, String firstName, String account, String risk) {
+		super();
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.account = account;
+		this.risk = risk;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getRisk() {
+		return risk;
+	}
+
+	public void setRisk(String risk) {
+		this.risk = risk;
+	}
+	
+	
+	
+	
+	
+}
