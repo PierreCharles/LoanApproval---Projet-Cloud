d7f38b7ed4e5a78a6f57583a12825f1928659f05
Pierre <pier.charles@hotmail.fr>
2016-05-01 12:10:43 +0200

    Refactoring all AppManager project

---
+++ AppManager/src/fr/appmanager/AppManager.java
@@ -0,0 +1,21 @@
+package fr.appmanager;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/approval")
+public class AppManager {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getBankAccount() {
+		return "{\"message\" : \"Hello\"}";
+	}
+
+	
+}
