dd75a167577963c1e0ebae250a7c2c166948fc3f
Pierre CHARLES <pier.charles@hotmail.fr>
2016-05-11 21:58:49 +0200

    Add calling accmanager service

--- checkaccount/src/main/java/fr/checkaccount/CheckAccount.java
+++ checkaccount/src/main/java/fr/checkaccount/CheckAccount.java
@@ -7,6 +7,14 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -15,43 +23,62 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;  
 
 /**
- * Root resource (exposed at "myresource" path)
+ * Root resource (exposed at "checkaccount" path)
  */
 @Path("checkaccount")
 public class CheckAccount {
 
-	
-	public final static int DATA_PRICE = 10000;
-	
+	public static final String urlAccManager = "http://1-dot-accmanager-1294.appspot.com/getAccount/";
+
 	/**
 	 * Converter to JSON
 	 */
 	private ObjectMapper converterJson = new ObjectMapper();
 	
 	/**
-	 * Method for check is an account is "low" or "hight" 
-	 * @param price
+	 * Method for check is an account is "low" or "hight" -> Call AccManager service
+	 * @param risk
 	 * @return Response Json 
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("checkrisk/{price}")
-	public Response checkRisk(@PathParam("price") String price) 
+	@Path("checkrisk/{idAccount}")
+	public Response checkRisk(@PathParam("idAccount") String price) 
 	{
-		int value = Integer.parseInt(price);
 		try 
 		{
-			String output = null;
-			if(value>DATA_PRICE)
-				output = converterJson.writeValueAsString("hight");
-			else
-				output = converterJson.writeValueAsString("low");
-			return Response.status(200).entity(output).build();
+			int id = Integer.parseInt(idAccount);
+			JSONObject json = readJsonFromUrl(urlAccManager+idAccount);
+			JSONObject output = new JSONObject();
+			json.put(response, json.get("risk"));
+			return Response.status(200).entity(output).build();s
 		} catch (Exception e) {
 			String output = "{'error':'"+e.getMessage()+"'}";	
 			return Response.status(204).entity(output).build();
 		} 
 	}
+	
+	
+	  private static String readAll(Reader rd) throws IOException {
+		    StringBuilder sb = new StringBuilder();
+		    int cp;
+		    while ((cp = rd.read()) != -1) {
+		      sb.append((char) cp);
+		    }
+		    return sb.toString();
+	  }
+
+	  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		    InputStream is = new URL(url).openStream();
+		    try {
+		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+		      String jsonText = readAll(rd);
+		      JSONObject json = new JSONObject(jsonText);
+		      return json;
+		    } finally {
+		      is.close();
+		    }
+	  }
 	
 	
     /**
@@ -62,6 +89,6 @@ public class CheckAccount {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getIt() {
-    	return "Default checkaccount path";
+    	return "";
     }
 }
