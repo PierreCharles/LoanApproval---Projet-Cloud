0aef5c50214d3a4afe38b8ee16a2f98a3304aa17
Pierre CHARLES <pier.charles@hotmail.fr>
2016-05-12 16:26:36 +0200

    Add new loanapproval project

--- loanapproval/src/main/java/fr/loanapproval/MyResource.java
+++ loanapproval/src/main/java/fr/loanapproval/MyResource.java
@@ -4,33 +4,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-
-
 @Path("myresource")
 public class MyResource {
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("checkrisk/{idAccount}")
-    public Response checkRisk(@PathParam("idAccount") String idAccount) 
-    {
-        try {
-            String output = "";
-            return Response.status(200).entity(output).build();
-
-        } catch (Exception e) {
-            String output = "{\"error\": \" "+e.getMessage()+" \"}";   
-            return Response.status(204).entity(output).build();
-        }
-
-    }
-
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -39,8 +18,8 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "";
+        return "Hello, Heroku!";
     }
 }
